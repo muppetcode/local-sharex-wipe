fce6ca094ab8837c7c6dd53ba927e36cc8849f7a
muppetcode <alexheathcote1@btinternet.com>
2021-03-12 19:19:14 +0000

    Create main.py
    Initial

---
+++ main.py
@@ -0,0 +1,17 @@
+import os, shutil, colorama
+from colorama import Fore, Style
+colorama.init(autoreset=True)
+
+user = os.getlogin()
+PATHC, PATHD = (f"C:\\Users\\{user}\\Documents\\ShareX\\Screenshots", f"D:\\Users\\{user}\\Documents\\ShareX\\Screenshots")
+
+if os.path.exists(PATHC):	
+	screenshot_path = os.path.join(PATHC)
+	shutil.rmtree(screenshot_path, ignore_errors=True)
+	print(f"{Fore.GREEN}Wiped")
+elif os.path.exists(PATHD):
+	screenshot_path = os.path.join(PATHD)
+	shutil.rmtree(screenshot_path, ignore_errors=True)
+	print(f"{Fore.GREEN}Wiped")
+else:
+	print(f"{Fore.RED}Screenshot folder not found")
